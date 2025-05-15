30dc9b559f78ab62667c106331ba68e40a6c2e07
Robin <robinadhikari777@gmail.com>
2025-05-15 12:37:51 -0700

    added SetOperations routing

--- discrete-project/src/App.jsx
+++ discrete-project/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/truth-table" element={<TruthTable />} />
+        <Route path="/basic-set-operations" element = {<SetOperations/>} />
       </Routes>
     </>
   );
