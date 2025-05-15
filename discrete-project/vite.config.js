09838de0d7c58034d5953374f055b3c45e0b4516
Robin <robinadhikari777@gmail.com>
2025-05-15 13:50:38 -0700

    set to default

--- discrete-project/vite.config.js
+++ discrete-project/vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'v
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // plugins: [react(),
-  //   tailwindcss(),
-  // ],
+  plugins: [react(),
+    tailwindcss(),
+  ],
 
   //removed tailwindcss() function due to error.
   //error -> when running, it was referencing to tailwind, but it is not installed
   //correction for GPT
-  plugins: [react()],
+  // plugins: [react()],
 })
