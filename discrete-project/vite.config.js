18d4a79a12f66dbbd4240df998888d54a0727b09
rafmojica <rafumojica@gmail.com>
2025-05-21 19:40:16 -0700

    fixed routing

--- discrete-project/vite.config.js
+++ discrete-project/vite.config.js
@@ -2,14 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
-
-  //removed tailwindcss() function due to error.
-  //error -> when running, it was referencing to tailwind, but it is not installed
-  //correction for GPT
-  // plugins: [react()],
 })
